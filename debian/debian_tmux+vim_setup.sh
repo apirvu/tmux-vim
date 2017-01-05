6a24df47befadbf642009ee3ea8df19516e695c2
Galbenus Adrian <adrian.galbenus@kaazing.com>
2017-01-06 00:09:06 +0200

    Update debian_tmux+vim_setup.sh

--- debian/debian_tmux+vim_setup.sh
+++ debian/debian_tmux+vim_setup.sh
@@ -7,8 +7,8 @@ exedir=$(readlink -f $(dirname $0))
 if [[ $(which vim) ]]; then
     echo "Vim is already installed."
 else
-    sudo apt-get -y update
-    sudo apt-get -y upgrade
+#    sudo apt-get -y update
+#    sudo apt-get -y upgrade
     sudo apt-get -y install vim-gnome
 fi
 
@@ -29,14 +29,24 @@ else
     #yum install xclip xsel
 fi
 
-#Install java8 if not installed yet
-if [[ $( java -version 2>&1 | grep -i version | cut -d'"' -f2 | cut -d'.' -f1-2) < 1.8 ]]; then
-    sudo sh -c 'echo "deb http://ppa.launchpad.net/webupd8team/java/ubuntu xenial main" | tee /etc/apt/sources.list.d/webupd8team-java.list'
-    sudo sh -c 'echo "deb-src http://ppa.launchpad.net/webupd8team/java/ubuntu xenial main" | tee -a /etc/apt/sources.list.d/webupd8team-java.list'
-    sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys EEA14886
-    sudo apt-get update
-    sudo apt-get -y install oracle-java8-set-default
-fi
+
+read -p "This step will install java 8 if you are using a lower version.
+	Are you sure you want to do this? (Y/N) " -n 1 -r
+echo    # (optional) move to a new line
+if [[ ! $REPLY =~ ^[Yy]$ ]]
+    then
+	[[ "$0" = "$BASH_SOURCE" ]] && exit 1 || return 1
+    else
+	#Install java8 if not installed yet
+	if [[ $( java -version 2>&1 | grep -i version | cut -d'"' -f2 | cut -d'.' -f1-2) < 1.8 ]]; then
+	    sudo sh -c 'echo "deb http://ppa.launchpad.net/webupd8team/java/ubuntu xenial main" | tee /etc/apt/sources.list.d/webupd8team-java.list'
+	    sudo sh -c 'echo "deb-src http://ppa.launchpad.net/webupd8team/java/ubuntu xenial main" | tee -a /etc/apt/sources.list.d/webupd8team-java.list'
+	    sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys EEA14886
+	    sudo apt-get update
+	    sudo apt-get -y install oracle-java8-set-default
+	fi
+
+    fi
 
 #Vim+TMUX dotfiles
 cp -r ../dotfiles/{.vimrc,.tmux.conf} ~/
